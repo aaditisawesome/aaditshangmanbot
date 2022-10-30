a8b9d820ed269f0cfc4c7ad982173be065b591f5
Aadit Bansal <aaditisawesome@gmail.com>
2022-10-30 16:06:17 -0700

    responed after giving hint

--- index.py
+++ index.py
@@ -382,6 +382,7 @@ async def buy(interaction, item: str, amount : int = 1):
                 await interaction.edit_original_response(content = 'You don\'t have any coins! Get coins by typing `/start` and win!')
                 return
             changeItem(interaction.user, "hint", amount)
+            await interaction.edit_original_response(content = "Success! You now have " + amount + " more hint(s).")
         except Exception as e:
             print(e)
             await interaction.edit_original_response(content = 'You don\'t have any coins! Get coins by typing `/start` and win!')
